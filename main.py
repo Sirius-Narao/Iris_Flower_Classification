084f594591fbfbae2eac21580253ec7c09a1e0ea
Yanis Aitali <aitaliyanis222@gmail.com>
2026-02-07 23:29:30 +0100

    lkkjjkkjkjkj

--- main.py
+++ main.py
@@ -11,4 +11,39 @@ from sklearn.preprocessing import MinMaxScaler
 
 data = datasets.load_iris()
 
-print(data)
+print(data.keys())
+
+df = pd.DataFrame(data.data, columns=data.feature_names)
+print(df)
+
+scaler = MinMaxScaler()
+scaler.fit(df)
+scaled = scaler.fit_transform(df)
+scaled_df = pd.DataFrame(scaled, columns=df.columns)
+
+
+scaled_df["target"] =data.target
+target_dict = dict(zip([0, 1, 2], data.target_names))
+scaled_df["target_name"] = scaled_df['target'].map(target_dict)
+print(target_dict)
+
+print(scaled_df)
+
+plt.figure(figsize=(7,7))
+plt.pie(scaled_df.target.value_counts(), labels=data.target_names, textprops={'fontsize':14.5}, colors=["r", "g", "b"], autopct = lambda x: '{p:.2f}% ({v:.1f})'.format(p=x, v = x*len(scaled_df)/100))
+
+print(scaled_df.dtypes)
+means = scaled_df.groupby("target").mean(numeric_only=True)
+print(means)
+
+features = means.columns
+x_axis = np.array([1, 2, 3, 4])
+y_axis = means.values
+plt.figure(figsize=(10, 5))
+plt.bar(x_axis-0.25, y_axis[0], width=0.25, color='r')
+plt.bar(x_axis, y_axis[1], width=0.25, color='b')
+plt.bar(x_axis+0.25, y_axis[2], width=0.25, color='g')
+plt.xticks(x_axis, features)
+plt.legend(data.target_names)
+sb.pairplot(scaled_df[])
+plt.show()
